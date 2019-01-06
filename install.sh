c60332ca0573de318359bec774126945412c6b99
Jiri Konecny <jkonecny@redhat.com>
2019-01-06 08:06:40 +0100

    Remove the original file with backup
    This behavior can be disabled by --noremove option.

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 CONFDIR=$(pwd)/confs
-
 BINDIR=$(pwd)/bin
+REMOVE=true
+
+if [ "$1" == "noremove" ]; then
+    REMOVE=false
+elif [ "$#" -gt 1 ]; then
+    echo "The only valid parameter is --noremove!" >&2
+    echo "--noremove  - Backup and remove existing files which are not symlinks." >&2
+    exit 2
+fi
+
 
 function install_file {
     if [ ! -e $2 ]; then
@@ -13,16 +22,37 @@ function install_file {
     if [ -h $1 ]; then
         echo "File $1 is a symbolic link. Skipping!"
     elif [ -e $1 ]; then
-        echo "File $1 already exists and it is NOT a symbolic link. Skipping!"
-    else
-	CONFDIR=$(dirname $1)
-	if [ ! -d $CONFDIR ]; then
-	    echo "Directory is missing -- creating!"
-	    mkdir -p $CONFDIR
+	if $REMOVE; then
+	    backup_and_remove $1
+	    create_symlink $1 $2
+	else
+            echo "File $1 already exists and it is NOT a symbolic link. Skipping!"
 	fi
+    else
+	create_symlink $1 $2
+    fi
+}
 
-        echo "Creating symbolic link to $2"
-        ln -s $2 $1
+function create_symlink {
+    original=$1
+    new=$2
+    ORIG_CONF_DIR=$(dirname $original)
+
+    if [ ! -d $ORIG_CONF_DIR ]; then
+        echo "Directory is missing -- creating!"
+        mkdir -p $ORIG_CONF_DIR
+    fi
+
+    echo "Creating symbolic link to $original"
+    ln -s $new $original
+}
+
+function backup_and_remove {
+    if [ -e $1 ]; then
+	mv $1 "$1.back"
+	echo "Backup $1.back created" >&2
+    else
+	echo "Nothing to backup and remove -- this is most probably bug in the code!" >&2
     fi
 }
 
@@ -35,10 +65,14 @@ function install_dir {
     if [ -h $1 ]; then
         echo "Directory $1 is a symbolic link. Skipping!"
     elif [ -d $1 ]; then
-        echo "Directory $1 already exists and it is NOT a symbolic link. Skipping!"
+	if $REMOVE; then
+	    backup_and_remove $1
+	    create_symlink $1 $2
+	else
+	    echo "Directory $1 already exists and it is NOT a symbolic link. Skipping!"
+	fi
     else
-        echo "Creating symbolic link to $2"
-        ln -s $2 $1
+	create_symlink $1 $2
     fi
 }
 
