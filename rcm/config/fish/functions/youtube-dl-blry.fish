69eac6eda9721c0b631a0c3fa7fa5954bdfe145e
Jiri Konecny <jkonecny@redhat.com>
2020-10-15 11:30:24 +0200

    Improve youtube-dl-lbry
    Get more info from this command.

--- rcm/config/fish/functions/youtube-dl-blry.fish
+++ rcm/config/fish/functions/youtube-dl-blry.fish
@@ -1,3 +1,3 @@
 function youtube-dl-blry --description "Download youtube videos in compatible format for BLRY"
-    youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=aac]/best" --merge-output-format mp4 --write-description "$argv"
+    youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=aac]/best" --merge-output-format mp4 --write-description --write-info-json --write-annotations "$argv"
 end
