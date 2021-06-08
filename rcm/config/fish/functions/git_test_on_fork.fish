788f61ef9bb3234610b7e837d49f496c5a1da11a
Jiri Konecny <jkonecny@redhat.com>
2021-06-08 14:58:18 +0200

    Improve git_test_on_fork.fish to allow specify file to change
    This is really handy when you need to test specific GH actions reaction on PR.

--- rcm/config/fish/functions/git_test_on_fork.fish
+++ rcm/config/fish/functions/git_test_on_fork.fish
@@ -1,9 +1,18 @@
 #!/bin/fish
 
 function git_test_on_fork --description "reset current HEAD to given repository and add test change"
-    if test 1 -ne (count $argv)
+    if test 0 -eq (count $argv)
         echo "Please specify source branch for 'reset --hard' call" >&2
+        echo "Help:" >&2
+        echo " git_test_on_fork <target_master_branch> [File to change]" >&2
+        return 2
     end
 
-    git reset --hard $argv[1]; and echo 'a' >> ./README.rst; and git commit -a -m "WIP - test commit"; and git push -f fork
+    set file_to_change "./README.rst"
+
+    if test 2 -eq (count $argv)
+        set file_to_change $argv[2]
+    end
+
+    git reset --hard $argv[1]; and echo 'a' >> $file_to_change; and git commit -a -m "WIP - test commit"; and git push -f fork
 end
