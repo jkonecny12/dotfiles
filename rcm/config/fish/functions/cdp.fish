6520e79dfa6404c32d27071aeac8761a83b3ea27
Jiri Konecny <jkonecny@redhat.com>
2021-06-09 11:04:40 +0200

    Improve cdp command to get easily to a project subfolder

--- rcm/config/fish/functions/cdp.fish
+++ rcm/config/fish/functions/cdp.fish
@@ -1,4 +1,8 @@
-function cdp --description "cd to projects directory"
-    cd ~/RH/projects/
+function cdp --description "cd to projects directory; you can pass argument to cd to a sub-directory"
+    if test 0 -ne (count $argv)
+      cd ~/RH/projects/$argv
+    else
+      cd ~/RH/projects/
+    end
 end
 
